82b088570962b12864c47f84704bb5a9763c3c5d
Sharbel Habchy <sharbelhabchy@gmail.com>
2023-10-13 10:52:35 -0500

    We now have pdfs and ttfs

--- mainFont.py
+++ mainFont.py
@@ -1,11 +1,17 @@
 import requests
 import customHashish as hash
-import pdfWriter as pw
+
+
+# from reportlab.pdfbase import pdfmetrics
+# from reportlab.pdfbase.ttfonts import TTFont
 
 # Define the URL for accessing the Shopify store's API
 url = 'https://a49ba18159fe5c18878c02cd077c773b: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@lovcompression.myshopify.com/admin/api/2022-04/'
 
+
 # Function to retrieve orders from the Shopify API
+
+
 def get_orders():
     endpoint = 'orders.json'
     r = requests.get(url + endpoint)
@@ -31,32 +37,38 @@ class Order:
 
     def printOrder(self):
         # print(self.orderID)
-        print(len(self.lineItems)) # print the line Item at index i
+        print(len(self.lineItems))  # print the line Item at index i
         for item in self.lineItems:
             print(item)
 
-"""
+
+"""|
 getSpecificProductID
 
 gets the product ID from a line item
 
 parameters:
     item: the line item - order["line_items"][0]["properties"]
-            
+
 output:
     returns the productID as a string
 """
+
+
 def getSpecificProductID(item):
     return int(item["product_id"])
 
+
 # initialize lists
 listOfOrders = []  # list of classess
-listOfItems = [] #all the line items that have 
+listOfItems = []  # all the line items that have
 
 """
 data search: iterates through the orders and makes a list of class Orders
 returns that list
 """
+
+
 def dataSearch():
     orderData = get_orders()
     i = 0
@@ -78,30 +90,69 @@ def dataSearch():
 
         listOfOrders.append(myOrder)
 
+
 dataSearch()
+
 
 def printer():
     for i in len(listOfOrders):
         listOfOrders[i].printOrder()
+
 
 """
 gets each item's product ID
 """
 for item in listOfItems:
     productID = getSpecificProductID(item)
+
     hash.getDataFromLineItem(productID, item)
+
     hash.listOfColorLists["White"]
+
+# for color in hash.colors:
+#     print(color)
+#     print("Length!: ",len(hash.listOfColorLists[color]))
+#     if(len(hash.listOfColorLists[color]) != 0):
+#         for i in range(len(hash.listOfColorLists[color])):
+#             hash.listOfColorLists[color][i].print()
 
 from reportlab.lib.pagesizes import letter
 from reportlab.pdfgen import canvas
+from reportlab.pdfbase import pdfmetrics
+from reportlab.pdfbase.ttfonts import TTFont
 
-def makePDFs():
-    for color in hash.listOfColorLists:
-        if(len(hash.listOfColorLists[color]) != 0):
-            c = canvas.Canvas("pdfs/" + color + ".pdf", pagesize=letter)
-            for i in range(len(hash.listOfColorLists[color])):
-                c.drawString(30, 750 - (10 * i), hash.listOfColorLists[color][i].text)
-                c.drawString(150, 750 - (10 * i), hash.listOfColorLists[color][i].font)
-            c.save()
+# Create a new PDF document
+c = canvas.Canvas("test.pdf", pagesize=letter)
 
-makePDFs()
+#registering fonts
+pdfmetrics.registerFont(TTFont('Marker', 'Bangers.ttf'))
+pdfmetrics.registerFont(TTFont('Tough', 'BlackOpsOne-Regular.ttf'))
+pdfmetrics.registerFont(TTFont('College', 'college.ttf'))
+pdfmetrics.registerFont(TTFont('Iceberg', 'Iceberg.ttf'))
+pdfmetrics.registerFont(TTFont('Roboto', 'Roboto-Bold.ttf'))
+pdfmetrics.registerFont(TTFont('Jersey', 'sportsjersey.ttf'))
+pdfmetrics.registerFont(TTFont('Asos', 'full_Pack_2025.ttf'))
+pdfmetrics.registerFont(TTFont('Adventure', 'SF Fedora.ttf'))
+pdfmetrics.registerFont(TTFont('Sporty', 'Calibri Bold.TTF'))
+pdfmetrics.registerFont(TTFont('Burny', 'Burny.ttf'))
+
+
+# Set the title and author metadata for the PDF (optional)
+# c.setTitle("My Basic PDF")
+# c.setAuthor("I am a huble slave")
+
+def makePDF(color):
+    c = canvas.Canvas(color + ".pdf", pagesize=letter)
+    # for color in hash.colors:
+    # print(hash.listOfColorLists["Black"])
+    if (len(hash.listOfColorLists[color]) != 0):
+        for i in range(len(hash.listOfColorLists[color])):
+            c.setFont(hash.listOfColorLists[color][i].font, 25)                         # setting the font
+            c.drawString(70, 750 - (40 * i), hash.listOfColorLists[color][i].text)      # putting text onto page
+            #c.drawString(110, 750 - (10 * i), hash.listOfColorLists[color][i].font)
+            #c.drawString(175, 750 - (40 * i), 'Tough')
+    c.save()
+
+
+makePDF("White")
+# print(hash.listOfColorLists["Black"][0].font)
--- pdfWriter.py
+++ pdfWriter.py
@@ -1,23 +1,9 @@
 from reportlab.lib.pagesizes import letter
 from reportlab.pdfgen import canvas
 
-
-# Create a new PDF document
-c = canvas.Canvas("test.pdf", pagesize=letter)
-
-# Set the title and author metadata for the PDF (optional)
-# c.setTitle("My Basic PDF")
-# c.setAuthor("I am a huble slave")
-
 def makePDF(color):
-    c = canvas.Canvas(color + ".pdf", pagesize=letter)
-    # for color in hash.colors:
-    
-    
-    c.drawString(100, 750, "Hell")
-
-# Save the PDF file
+    c = canvas.Canvas("pdfs/" + color + ".pdf", pagesize=letter)
+    for i in range(len(hash.listOfColorLists[color])):
+            c.drawString(30, 750 - (10 * i), hash.listOfColorLists[color][i].text)
+            c.drawString(150, 750 - (10 * i), hash.listOfColorLists[color][i].font)
     c.save()
-
-makePDF("Black")
-
